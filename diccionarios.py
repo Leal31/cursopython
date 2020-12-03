e93e2f9f359216e08b5eb9ba1a297c3498591113
Leal31 <lealemanuel31@gmail.com>
2020-12-03 15:45:10 -0500

    segundo commit

--- diccionarios.py
+++ diccionarios.py
@@ -2,3 +2,16 @@
 diccionario = {"azul": "blue", "rojo": "red", "verde": "green"}
 
 print(diccionario)
+#solo un elemento del diccionario
+print(diccionario["azul"])
+#añadir un elemento a un diccionario
+diccionario["amarillo"] = "yellow"
+print(diccionario)
+#borrar un elemento del diccionario
+del(diccionario["verde"])
+
+print(diccionario)
+
+#combinar un elemento
+diccionario = {"Emanuel":[22, 1.72], "Jose":[21, 1.75]}
+print(diccionario)
