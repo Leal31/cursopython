76407f2e80ba78ab7479124f246c9e45feaafd78
Emanuel Leal Arce <lealemanuel31@gmail.com>
2020-12-25 13:40:51 -0500

    otro commit

--- diccionarios.py
+++ diccionarios.py
@@ -1,6 +1,5 @@
 #Diccionarios
 diccionario = {"azul": "blue", "rojo": "red", "verde": "green"}
-
 print(diccionario)
 #solo un elemento del diccionario
 print(diccionario["azul"])
@@ -9,9 +8,23 @@ diccionario["amarillo"] = "yellow"
 print(diccionario)
 #borrar un elemento del diccionario
 del(diccionario["verde"])
-
 print(diccionario)
-
 #combinar un elemento
-diccionario = {"Emanuel":[22, 1.72], "Jose":[21, 1.75]}
+diccionario = {"Emanuel":{"edad":22,"estatura": 1.72}, "Jose":[21, 1.75], "Maria":[22,1.66]}
 print(diccionario)
+print(diccionario["Emanuel"])
+#diccionario parte 2
+equipo = {10: "Paulo Dybala", 11: "Douglas Costa", 7:"Cristiano Ronaldo", 17: "Mario Mandzukic"}
+print(equipo)
+#Solo un valor
+print(equipo[10])
+print(equipo.get(14, "No existe un jugador con ese dorsal"))
+#saber si hay el valor en el diccionario
+print(10 in equipo)
+#Saber todas las claves del diccionario
+print(equipo.keys())
+#Saber los valores del diccionario
+print(equipo.values())
+#como saber valores del diccionario
+print(equipo.items())
+
